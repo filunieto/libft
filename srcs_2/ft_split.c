6f9481ce633c5ecc0bc84136ba9b2c9f4b6aaef9
filunie <filunie@gmail.com>
2022-02-04 11:18:18 +0100

    subida de archivos para probar los test

--- srcs_2/ft_split.c
+++ srcs_2/ft_split.c
@@ -6,12 +6,18 @@
 /*   By: fnieves <fnieves@42heilbronn.de>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 21:51:32 by fnieves           #+#    #+#             */
-/*   Updated: 2022/02/03 21:51:59 by fnieves          ###   ########.fr       */
+/*   Updated: 2022/02/04 00:23:32 by fnieves          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*recursión de strchr.c?
+vamos recorriendo s, buscando los carateres. 
+una vez  localizados, necesitamos apuntar los lugares para reservar memorias con malloc
+alojar los acaracteres en los espacioes de memoria reservados
+se hace free?
+*/
 char	**ft_split(char const *s, char c)
 {
 	
